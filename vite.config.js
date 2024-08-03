e2e902c713e894fb9d32c1f5a30c0e874882ca1e
Suraj Prajapat <>
2024-08-04 00:12:12 +0530

    vite configuration changed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext' //browsers can handle the latest ES features
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true //browsers can handle top-level-await features
+    },
+  }
 })
